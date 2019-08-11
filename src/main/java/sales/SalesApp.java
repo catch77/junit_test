c9a489213e06c7a22698f94156cf64e4db00925b
catch77 <1297056296@qq.com>
2019-08-11 23:24:40 +0800

    Refactor the code && add testGetSales && add testIsSalesOutOfDate

--- src/main/java/sales/SalesApp.java
+++ src/main/java/sales/SalesApp.java
@@ -8,27 +8,59 @@ import java.util.List;
 public class SalesApp {
 
 	public void generateSalesActivityReport(String salesId, int maxRow, boolean isNatTrade, boolean isSupervisor) {
-		
+
 		SalesDao salesDao = new SalesDao();
 		SalesReportDao salesReportDao = new SalesReportDao();
 
-		
+
 		List<SalesReportData> filteredReportDataList = new ArrayList<SalesReportData>();
 		
 		if (salesId == null) {
 			return;
 		}
+
+		Sales sales = getSalesBySalesId(salesId, salesDao);
+
+		if (isSalesOutOfDate(sales)) return;
+
+		List<SalesReportData> reportDataList = getSalesReportData(isSupervisor, salesReportDao, filteredReportDataList, sales);
+
+		getFilteredReportDateList(maxRow, reportDataList);
+
+		List<String> headers = getHeaders(isNatTrade);
+
+		SalesActivityReport report = this.generateReport(headers, reportDataList);
 		
-		Sales sales = salesDao.getSalesBySalesId(salesId);
+		EcmService ecmService = new EcmService();
+		ecmService.uploadDocument(report.toXml());
 		
+	}
+
+	protected Sales getSalesBySalesId(String salesId, SalesDao salesDao) {
+		return salesDao.getSalesBySalesId(salesId);
+	}
+
+	protected boolean isSalesOutOfDate(Sales sales) {
 		Date today = new Date();
 		if (today.after(sales.getEffectiveTo())
 				|| today.before(sales.getEffectiveFrom())){
-			return;
+			return true;
 		}
-		
+		return false;
+	}
+
+	protected void getFilteredReportDateList(int maxRow, List<SalesReportData> reportDataList) {
+		List<SalesReportData> filteredReportDataList;
+		List<SalesReportData> tempList = new ArrayList<SalesReportData>();
+		for (int i=0; i < reportDataList.size() || i < maxRow; i++) {
+			tempList.add(reportDataList.get(i));
+		}
+		filteredReportDataList = tempList;
+	}
+
+	protected List<SalesReportData> getSalesReportData(boolean isSupervisor, SalesReportDao salesReportDao, List<SalesReportData> filteredReportDataList, Sales sales) {
 		List<SalesReportData> reportDataList = salesReportDao.getReportData(sales);
-		
+
 		for (SalesReportData data : reportDataList) {
 			if ("SalesActivity".equalsIgnoreCase(data.getType())) {
 				if (data.isConfidential()) {
@@ -40,20 +72,7 @@ public class SalesApp {
 				}
 			}
 		}
-		
-		List<SalesReportData> tempList = new ArrayList<SalesReportData>();
-		for (int i=0; i < reportDataList.size() || i < maxRow; i++) {
-			tempList.add(reportDataList.get(i));
-		}
-		filteredReportDataList = tempList;
-
-		List<String> headers = getHeaders(isNatTrade);
-
-		SalesActivityReport report = this.generateReport(headers, reportDataList);
-		
-		EcmService ecmService = new EcmService();
-		ecmService.uploadDocument(report.toXml());
-		
+		return reportDataList;
 	}
 
 	protected List<String> getHeaders(boolean isNatTrade) {
@@ -66,7 +85,7 @@ public class SalesApp {
 		return headers;
 	}
 
-	private SalesActivityReport generateReport(List<String> headers, List<SalesReportData> reportDataList) {
+	protected SalesActivityReport generateReport(List<String> headers, List<SalesReportData> reportDataList) {
 		// TODO Auto-generated method stub
 		return null;
 	}
--- src/test/java/sales/SalesAppTest.java
+++ src/test/java/sales/SalesAppTest.java
@@ -1,22 +1,37 @@
 package sales;
 
-import mockit.Mock;
+import com.sun.org.apache.bcel.internal.generic.FADD;
+import mockit.MockUp;
+import mockit.integration.junit4.JMockit;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import static org.mockito.Mockito.*;
 
+@RunWith(MockitoJUnitRunner.class)
 public class SalesAppTest {
 
+	@InjectMocks
+	SalesApp salesApp;
+	@Mock
+	SalesDao salesDao;
+	@Mock
+	SalesReportDao salesReportDao;
+	@Mock
+	EcmService ecmService;
 
 	@Test
 	public void testGenerateReport() {
-		
 		SalesApp salesApp = mock(SalesApp.class);
 		salesApp.generateSalesActivityReport("DUMMY", 1000, false, false);
-		
 	}
 
 	@Test
@@ -26,5 +41,26 @@ public class SalesAppTest {
 		Assert.assertEquals("Local Time", headers.get(3));
 	}
 
-	
+	@Test
+	public void testGetSales_givenSalesId_thenReturnSales() {
+		Sales sales = mock(Sales.class);
+		when(salesDao.getSalesBySalesId(anyString())).thenReturn(sales);
+		Sales sales1 = salesDao.getSalesBySalesId("DUMMY");
+		Assert.assertEquals(sales, sales1);
+	}
+
+	@Test
+	public void testIsSalesOutOfDate_givenStartDateIsThreeDaysAgoAndEndDateIsYesterday_thenReturnTrue() {
+		Calendar startDate = Calendar.getInstance();
+		startDate.add(Calendar.DATE, -3);
+
+		Calendar endDate = Calendar.getInstance();
+		endDate.add(Calendar.DATE, -1);
+
+		Sales sales = mock(Sales.class);
+		when(sales.getEffectiveFrom()).thenReturn(startDate.getTime());
+		when(sales.getEffectiveTo()).thenReturn(endDate.getTime());
+		Assert.assertEquals(true, salesApp.isSalesOutOfDate(sales));
+	}
+
 }
